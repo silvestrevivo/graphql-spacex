07121b242eeb70cb8faab3b0613160c20369b653
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-01 23:05:13 +0100

    queries created from server api using graphql schemas

--- schema.js
+++ schema.js
@@ -41,7 +41,34 @@ const RootQuery = new GraphQLObjectType({
         return axios.get('https://api.spacexdata.com/v3/launches/')
           .then(res => res.data)
       }
-    }
+    },
+    launch: {
+      type: LaunchType,
+      args: {
+        flight_number: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+          .then(res => res.data)
+      }
+    },
+    rockets: {
+      type: new GraphQLList(RocketType),
+      resolve(parent, args) {
+        return axios.get('https://api.spacexdata.com/v3/rockets/')
+          .then(res => res.data)
+      }
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        rocket_id: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        return axios.get(`https://api.spacexdata.com/v3/rockets/${args.rocket_id}`)
+          .then(res => res.data)
+      }
+    },
   })
 })
 
