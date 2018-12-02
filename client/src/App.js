4f2131fde48ddc804c6585c801b611bcb0d90f9b
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-02 11:05:08 +0100

    apollo client set up on react application

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
 import logo from './logo.png';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={logo} alt="logo" style={{width: 300, display: 'block', margin: 'auto'}}/>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img src={logo} alt="logo" style={{ width: 300, display: 'block', margin: 'auto' }} />
+        </div>
+      </ApolloProvider>
     );
   }
 }
